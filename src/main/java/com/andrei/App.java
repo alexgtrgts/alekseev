716793aa8f2e07fc05164b07098e452700c403bc
Andrei <andrei.alekseeff@gmail.com>
2021-03-25 21:00:58 +0300

    commiting new file

---
+++ src/main/java/com/andrei/App.java
@@ -0,0 +1,13 @@
+package com.andrei;
+
+/**
+ * Hello Git,again!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Git,again!" );
+    }
+}
