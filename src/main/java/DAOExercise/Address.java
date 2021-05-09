59f7192351452abf3a66f175b1a907fba259c9bb
Andrei <andrei.alekseeff@gmail.com>
2021-05-09 17:41:47 +0300

    DAO with lombok

--- src/main/java/DAOExercise/Address.java
+++ src/main/java/DAOExercise/Address.java
@@ -1,51 +1,17 @@
 package DAOExercise;
 
+import jdk.jfr.DataAmount;
+import lombok.*;
+
 import java.util.Objects;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 
 public class Address {
 
     private String street;
     private int house;
 
-    public Address(String street, int house) {
-        this.street = street;
-        this.house = house;
-    }
-
-    @Override
-    public String toString() {
-        return "Address{" +
-                "street='" + street + '\'' +
-                ", house=" + house +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Address address = (Address) o;
-        return house == address.house && Objects.equals(street, address.street);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(street, house);
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public int getHouse() {
-        return house;
-    }
-
-    public void setHouse(int house) {
-        this.house = house;
-    }
 }
--- src/main/java/DAOExercise/People.java
+++ src/main/java/DAOExercise/People.java
@@ -1,62 +1,18 @@
 package DAOExercise;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import java.util.Objects;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class People {
 
     private String name;
     private String surname;
     private int age;
+    private Address address;
 
-    public People( String name, String surname, int age) {
-        this.name = name;
-        this.surname = surname;
-        this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return "People{" +
-                ", name='" + name + '\'' +
-                ", surname='" + surname + '\'' +
-                ", age=" + age +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        People people = (People) o;
-        return age == people.age && Objects.equals(name, people.name) && Objects.equals(surname, people.surname);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, surname, age);
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
 }
